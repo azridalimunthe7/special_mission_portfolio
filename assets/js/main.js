be4cb7be83216043bed5218f8b046d02f34bb133
azridalimunthe7 <azridalimunthe@gmail.com>
2026-02-08 22:23:54 +0700

    menambahkan certificate

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,9 +53,12 @@ const sr = ScrollReveal({
   //     reset: true
 });
 
-sr.reveal(".home__data, .about__img, .skills__subtitle, .skills__text", {});
+sr.reveal(
+  ".home__data, .about__img, .skills__subtitle, .skills__text, .project-card, .certificate-container",
+  {},
+);
 sr.reveal(".home__img, .about__subtitle, .about__text, .skills__img", {
   delay: 400,
 });
 sr.reveal(".home__social-icon", { interval: 200 });
-sr.reveal(".skills__data, .project-card, .contact__input", { interval: 200 });
+sr.reveal(".skills__data, .contact__input", { interval: 200 });
